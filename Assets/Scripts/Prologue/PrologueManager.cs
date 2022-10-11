e4762b5cb883e65eea6204eebad1cf7c33b5ebba
duoni-o <57888009+duoni-o@users.noreply.github.com>
2022-10-11 12:55:41 +0900

    프롤로그 씬 기본 틀 구현_01

--- Assets/Scripts/Prologue/PrologueManager.cs
+++ Assets/Scripts/Prologue/PrologueManager.cs
@@ -3,21 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
+/*
+ * 진행 방식 
+ * 1. Newspaper 떠 있는 상태로 시작
+ * 2. 엔터 후 Newpaper.SetActive(false). Newspaper 다이얼로그 재생
+ * 3. 재생 종료 후 신청서 등장과 함께 ApplicationForm01 다이얼로그 재생
+ * 4. 신청서 작성 후 submit 누르면 ApplicationForm02 다이얼로그 재생
+*/
+
 public class PrologueManager : MonoBehaviour
 {
-    private IEnumerator coroutine;
+    // 상태
+    public bool isNewpaperActive = true;
 
-    private void Start()
+    // 변수
+    public GameObject newspaper;
+    public GameObject applForm;
+
+    public void StartNewspaperDial()
     {
-        coroutine = MoveToApplicationForm();
-        StartCoroutine(coroutine);
+        if (Input.GetKeyDown(KeyCode.Return) && newspaper.activeSelf == true)
+        {
+            // node Newspaper 재생
+
+            // 신문기사 비활성화
+            newspaper.SetActive(false);
+        }
+        isNewpaperActive = false;
     }
 
-    [YarnCommand("custom_wait")]
-    private IEnumerator MoveToApplicationForm()
+    public void StartAppForm1Dial()
     {
-        yield return new WaitForSeconds(0.1f);
+        // 앞에 다이얼 끝나면(이건 인스펙터 창에 있음)
+        // 신청서 활성화
+        GameObject.Find("Canvas").transform.Find("ApplicationForm").gameObject.SetActive(true);
+        // node ApplicationForm01 재생
 
     }
 
+    public void StartAppForm2Dial()
+    {
+        // 버튼 클릭했을 때
+        // node ApplicationForm02 재생
+    }
 }
