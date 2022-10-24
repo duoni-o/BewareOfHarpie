e033ed542b527dba8bc74e3cefce618d50d32a4f
duoni-o <57888009+duoni-o@users.noreply.github.com>
2022-10-25 08:21:17 +0900

    프롤로그 구현 완료

--- Assets/Scripts/YarnInteractable.cs
+++ Assets/Scripts/YarnInteractable.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class YarnInteractable : MonoBehaviour {
     [SerializeField] private string conversationStartNode;
 
+    //[SerializeField]
+    //Image fadeOutImage;
+
     private DialogueRunner dialogueRunner;
     private bool interactable = true;
     private bool isCurrentConversation = false;
     private float defaultIndicatorIntensity;
-
-    [SerializeField]
-    GameObject newspaper;
 
     public void Start() {
         dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
@@ -23,6 +24,11 @@ public class YarnInteractable : MonoBehaviour {
     public void OnMouseDown() {
         if (interactable && !dialogueRunner.IsDialogueRunning) {
             StartConversation();
+            if (name == "Player_broken")
+            {
+                GameObject.Find("Player_broken").gameObject.SetActive(false);
+                GameObject.Find("Canvas").transform.Find("Player_broken_png").gameObject.SetActive(true);
+            }
         }
     }
 
@@ -38,11 +44,22 @@ public class YarnInteractable : MonoBehaviour {
             Debug.Log($"End conversation with {name}.");
             if (name == "Newspaper")
             {
-                newspaper.SetActive(false);
+                GameObject.Find("Newspaper").gameObject.SetActive(false);
                 GameObject.Find("Canvas").transform.Find("ApplicationForm").gameObject.SetActive(true);
             }
             if (name == "Submit")
             {
+                GameObject.Find("Canvas").transform.Find("Submit_completed").gameObject.SetActive(true);
+            }
+            if (name == "OK")
+            {
+                // ÆäÀÌµå¾Æ¿ô
+                //StartCoroutine(FadeOutCoroutine());
+                SceneManager.LoadScene("Prologue_dump");
+            }
+            if (name == "Player_broken")
+            {
+                // ÆäÀÌµå¾Æ¿ô
                 SceneManager.LoadScene("Title");
             }
         }
@@ -52,4 +69,16 @@ public class YarnInteractable : MonoBehaviour {
     public void DisableConversation() {
         interactable = false;
     }
+
+    /*IEnumerator FadeOutCoroutine()
+    {
+        GameObject.Find("Canvas").transform.Find("FadeOutImage").gameObject.SetActive(true);
+        float fadeCount = 0;
+        while (fadeCount < 1.0f)
+        {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            fadeOutImage.color = new Color(0, 0, 0, fadeCount);
+        }
+    }*/
 }
