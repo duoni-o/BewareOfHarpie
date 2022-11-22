1e001e95e86356c82c3cb44227849f1c480b1add
SEOEUN <87869785+seoeunkong@users.noreply.github.com>
2022-11-22 11:20:20 +0900

    씬 이동에 관한 데이터 저장

--- Assets/Scripts/DataSave/PlayerDataSave.cs
+++ Assets/Scripts/DataSave/PlayerDataSave.cs
@@ -36,8 +36,8 @@ public class PlayerDataSave : MonoBehaviour
     //해당 아이템을 획득했다면 비활성화하기
     void ItemActive(){
 
-      //획득해야하는 아이템들
-      string[] itemArray = {"Key","Cotton","Button","Thread","Needle","Note"};
+      //획득해야하는 아이템들 및 탈출한 장소
+      string[] itemArray = {"Key","Cotton","Dump","Button","Thread","Needle","Note"};
       //획득한 아이템 리스트
       List<string> equipItemsList = new List<string>();
       
@@ -49,6 +49,7 @@ public class PlayerDataSave : MonoBehaviour
         }
       }
 
+      //획득한 아이템 비활성화하기
       for(int i=0; i<equipItemsList.Count; i++){
         if(equipItemsList[i] == "Key"){
           key.SetActive(false);
